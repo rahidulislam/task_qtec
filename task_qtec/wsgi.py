21fc5552f811fee7e941fb4e001db321d26f34d0
Rahidul Islam <rahiseli@outlook.com>
2023-05-27 14:58:27 +0600

    change setting

--- task_qtec/wsgi.py
+++ task_qtec/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_qtec.settings')
 
-app = get_wsgi_application()
-# app = application
+application = get_wsgi_application()
+app = application
