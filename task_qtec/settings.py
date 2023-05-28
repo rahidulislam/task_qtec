db679c569f1dbb42f03c378af50b3c0518214864
Rahidul Islam <rahiseli@outlook.com>
2023-05-28 22:48:44 +0600

    postgresql port is updated

--- task_qtec/settings.py
+++ task_qtec/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': config('POSTGRES_HOST'),
-        'PORT': config('PGPORT'),
+        'PORT': '5432',
     }
 }
 
