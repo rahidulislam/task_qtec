9751c7e3de1960e5988d7a49b68b181bd42eb5b3
Rahidul Islam <rahiseli@outlook.com>
2023-05-28 22:42:24 +0600

    vercel is updated

---
+++ build_files.sh
@@ -0,0 +1,9 @@
+echo "===Install Requirements===="
+pip install -r requirements.txt
+
+echo "===Collect Static Files==="
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "===Make Migration==="
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
--- task_qtec/settings.py
+++ task_qtec/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
